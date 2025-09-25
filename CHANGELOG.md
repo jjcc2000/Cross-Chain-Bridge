308ec6c9a722b6a4c846b360dc3df00ee64e2a97
esJohan <johanchaconchacon@gmail.com>
2025-09-25 15:16:19 -0600

    ready to deploy with CCT

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+All notable changes to this project will be documented here.
+
+The v2 is set to use the Cross-Chain Token [Cross-Chain-Token](https://docs.chain.link/ccip/concepts/cross-chain-token)
--- contracts/src/interfaces/IBridgedERC20.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-
-interface IBridgedERC20 {
-    function mint(address to, uint256 amount) external;
-    function burn(address from, uint256 amount) external;
-}
